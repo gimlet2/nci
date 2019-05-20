31cff844e30a74abb2cec6238ebe71bcbe05174b
Andrei Chernyshev <andrei.chernyshev@outfittery.de>
2019-05-20 17:28:04 +0200

    move http_utils

--- http_utils.go
+++ http_utils.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func Json(w http.ResponseWriter, v interface{}) {
@@ -21,4 +25,39 @@ func Error(w http.ResponseWriter, e error) {
 	log.Printf("Failed to get resourse %v", e)
 	w.WriteHeader(500)
 	w.Write([]byte("Error"))
+}
+
+func Server(init func(r *mux.Router)) {
+	r := mux.NewRouter()
+	init(r)
+	srv := &http.Server{
+		Addr: "0.0.0.0:" + getPort(),
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return port
+}
+
+func Get(r *mux.Router, pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	request(r, pattern, handler, "GET")
+}
+
+func Post(r *mux.Router, pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	request(r, pattern, handler, "POST")
+}
+
+func request(r *mux.Router, pattern string, handler func(w http.ResponseWriter, r *http.Request), method string) {
+	r.HandleFunc(pattern, handler).Methods(method)
 }
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"time"
 
 	"net/http"
 
@@ -12,38 +10,13 @@ import (
 
 func main() {
 	log.Println("Application started")
-	r := mux.NewRouter()
-	get(r, "/", func(w http.ResponseWriter, r *http.Request) {
-		Json(w, "hello")
+
+	Server(func(r *mux.Router) {
+		Get(r, "/", func(w http.ResponseWriter, r *http.Request) {
+			Json(w, "hello")
+		})
 	})
-
-	srv := &http.Server{
-		Addr: "0.0.0.0:" + getPort(),
-		// Good practice to set timeouts to avoid Slowloris attacks.
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		IdleTimeout:  time.Second * 60,
-		Handler:      r, // Pass our instance of gorilla/mux in.
-	}
-	log.Fatal(srv.ListenAndServe())
 }
 
-func get(r *mux.Router, pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
-	request(r, pattern, handler, "GET")
-}
 
-func post(r *mux.Router, pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
-	request(r, pattern, handler, "POST")
-}
 
-func request(r *mux.Router, pattern string, handler func(w http.ResponseWriter, r *http.Request), method string) {
-	r.HandleFunc(pattern, handler).Methods(method)
-}
-
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	return port
-}
